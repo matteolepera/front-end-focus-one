beaf6e1afd11f02daf16787166b93f6d479b887a
matteolpera <matteo.lepera02@gmail.com>
2026-04-29 23:17:55 +0200

    fix: aggiunto l'import della pagina not found alla pagina TeamDetails.

--- src/pages/TeamDetails.jsx
+++ src/pages/TeamDetails.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
 
 export default function TeamDetails({ setLoading }) {
 
