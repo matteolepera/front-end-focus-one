ae704d7e01bf1b3a53a21739348ff30e24335b20
matteolpera <matteo.lepera02@gmail.com>
2026-05-03 17:38:46 +0200

    feat: aggiunta pagina not found.
    rivista biografica pagina singolo pilota.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import styles from "../styles/NotFound.module.css"
 
 export default function NotFound() {
     return (
-        <div style={{ textAlign: "center", padding: "50px" }}>
-            <h1>404</h1>
-            <h2>Page Not Found</h2>
-
-            <p>La pagina che stai cercando non esiste.</p>
-
-            <Link to="/">
+        <div className={styles.page}>
+            <h1 className={styles.code}>404</h1>
+            <h2 className={styles.title}>Page Not Found</h2>
+            <div className={styles.divider} />
+            <p className={styles.description}>
+                La pagina che stai cercando non esiste.
+            </p>
+            <Link to="/" className={styles.link}>
                 ← Torna alla Home
             </Link>
         </div>
